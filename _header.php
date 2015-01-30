2578ec17d0946b8f18c94a2dbf6e04dc9c03a7f7
Jimmy <jimmylorunning@gmail.com>
2015-01-30 12:45:28 -0500

    Change google fonts to https for heroku compatibility

--- _header.php
+++ _header.php
@@ -12,8 +12,8 @@
 
         <link rel="stylesheet" href="css/normalize.min.css">
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-        <link href='http://fonts.googleapis.com/css?family=VT323' rel='stylesheet' type='text/css'>
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=VT323' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
         <?php
         if (!isset($css)) {$css = 'main.css';}
         ?>
