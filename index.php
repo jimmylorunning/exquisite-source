fdc12cdb3870e4370dc03cd538912199ec3c0abe
Jimmy <jimmylorunning@gmail.com>
2015-02-07 22:11:32 -0500

    Extracted homepage contents into html file for easy editing. Even if you don't have php set up, you should be able to view and edit homepage.html. I still want the php there so that I can include the same headers and footers and styles for every page

--- index.php
+++ index.php
@@ -2,26 +2,7 @@
 <div class="main-container">
     <div class="main wrapper clearfix">
 
-        <article>
-            <header>
-                <p>I wasn't surprised when I walked in to find everyone as confused as i am </p>
-                <p>when a trap door opened in the ceiling, and out fell<p>
-                <p>a brown paper bag containing a half eaten burrito. Attached to the burrito was a mysterious note instructing me to<p>
-            </header>
-            <section>
-                <h2></h2>
-                <p>...</p>
-            </section>
-            <section>
-                <h2></h2>
-                <p>...</p>
-            </section>
-        </article>
-
-        <aside>
-            <h3>Meanwhile...</h3>
-            <p>Some guy who normally uses Ruby searches Google for what web server is needed to view PHP files locally and finds.... </p>
-        </aside>
+       <?php include 'homepage.html' ?>
 
     </div> <!-- #main -->
 </div> <!-- #main-container -->
